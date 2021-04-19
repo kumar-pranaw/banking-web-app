7ac7e591d82c1fdd7b72df82b4a5a4be76ccd37f
Pranav <prnvkmr954@gmail.com>
2021-04-20 00:31:10 +0530

    Initialized project

---
+++ banking-web-app/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'banking-web-app';
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    console.log(this.getTitle());
+  }
+
+  getTitle() {
+    return this.title;
+  }
+
+}
